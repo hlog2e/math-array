26229a76bbe3576496e765878e0582cf00483168
HongRok Kim <hlog2e@gmail.com>
2022-06-09 05:35:33 +0900

    Initial Commit

---
+++ math-array/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  math-array
+//
+//  Created by HongRok Kim on 2022/06/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
