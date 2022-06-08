22fbefc5e0c447899e351f2568d8d386c618a943
HongRok Kim <hlog2e@gmail.com>
2022-06-09 06:29:27 +0900

    기본적인 계산 로직 완성

--- math-array/main.c
+++ math-array/main.c
@@ -1,14 +1,61 @@
-//
-//  main.c
-//  math-array
-//
-//  Created by HongRok Kim on 2022/06/09.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void arithmeticProgression() {
+    int startValue, gongCha, hang;
+    printf("---------등차수열 계산---------\n");
+    printf("수열의 시작을 알려주세요.\n");
+    scanf("%d",&startValue);
+    printf("공차를 알려주세요.\n");
+    scanf("%d",&gongCha);
+    printf("몇 번째 항까지 구해볼까요?.\n");
+    scanf("%d",&hang);
+    for(int i=0; i < hang ; i++ ){
+        printf(" %d", startValue);
+        startValue = startValue + gongCha;
+    }
+    
+}
+
+void geometricSequence() {
+    long long startValue;
+    int  gongBee, hang;
+    printf("---------등비수열 계산---------\n");
+    printf("수열의 시작을 알려주세요.\n");
+    scanf("%lld",&startValue);
+    printf("공비를 알려주세요.\n");
+    scanf("%d",&gongBee);
+    printf("몇 번째 항까지 구해볼까요?.\n");
+    scanf("%d",&hang);
+    for(int i=0; i < hang ; i++ ){
+        printf(" %lld", startValue);
+        startValue = startValue * gongBee;
+    }
+}
+
+
+int main() {
+    
+    int selection;
+    
+    
+    printf("---------등차,등비 수열 계산기---------\n");
+    printf("수열의 종류를 선택해주세요.\n");
+    printf("1:등차수열\n");
+    printf("2:등비수열\n");
+    scanf("%d",&selection);
+    
+    switch (selection) {
+        case 1:
+            arithmeticProgression();
+            break;
+        case 2: geometricSequence();
+            break;
+            
+    }return 0;
+    
+    
+    
+    
     return 0;
 }
+
